d4861d2a80de2651d2bf8326359d2a7d10f97b24
Gonzalo Diaz Morilla <gdiaz4@hawk.iit.edu>
2023-02-07 11:04:25 -0800

    Update uppyScript.js

--- uppyScript.js
+++ uppyScript.js
@@ -96,7 +96,7 @@ submitButton.addEventListener('click', () => {
 
 
 uppy.on('file-added', (file) => {
- const UppyInput = document.querySelector('.UppyInput')
+ const UppyInput = document.querySelector('.uppy-input')
 
  const paragraph = document.createElement("P");
  paragraph.className = 'UppyInputStatus';
